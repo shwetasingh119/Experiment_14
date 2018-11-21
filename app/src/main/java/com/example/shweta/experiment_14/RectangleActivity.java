9574f4fbb8a5003103a67a6242b4da313f912381
Experiment_1 <https://github.com/shwetasingh119/Experiment_1.git>
2018-11-21 18:18:08 +0530

    Initial

---
+++ app/src/main/java/com/example/shweta/experiment_14/RectangleActivity.java
@@ -0,0 +1,13 @@
+package com.example.shweta.experiment_14;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RectangleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rectangle);
+    }
+}
